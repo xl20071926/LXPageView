0af54b391791212490b558f4210d539edaa9e63f
xl20071926 <lixin_1990@yeah.net>
2017-03-17 09:44:08 +0800

    Initial Commit

---
+++ LXPageView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LXPageView
+//
+//  Created by Leexin on 17/3/17.
+//  Copyright © 2017年 Garden.Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
